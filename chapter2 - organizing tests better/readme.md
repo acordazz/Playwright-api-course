143420cc9768227f8b26a8a168a4e86e4262235b
André Cordazzo Brunelli <andre.cordazzo-brunelli@sogeti.no>
2023-03-24 13:39:23 +0100

    done with chapter 2 for now

--- chapter2 - organizing tests better/readme.md
+++ chapter2 - organizing tests better/readme.md
@@ -1,2 +1,12 @@
 # Chapter 2: Organizing your tests
 
+Continuing with the API:
+https://restful-booker.herokuapp.com/apidoc/index.html
+
+The code has some enhancements comparing to the previous chapter. We now have some additional ways to organize our code:
+- test.describe(): allows us to group some tests according to any reason we want. An example would be to configure a group of tests to run in parallel, or with a specific number of retries. In addition we can use all types of test.use() configurations (ex: a specific locale such as 'en-US')
+- test.beforeAll(), test.beforeEach(), test.afterAll(), test.afterEach(): similar to other unit test frameworks.
+- test.skip(): can be used to skip tests depending on the browser for example
+- test.fail(): if you don't have a finished test yet you can keep it failed until you fix it. Similar concept to Red-Green-Refactor in TDD
+  
+  See: https://playwright.dev/docs/api/class-test
--- chapter2 - organizing tests better/tests/example.spec.ts
+++ chapter2 - organizing tests better/tests/example.spec.ts
@@ -1,18 +1,135 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+test.describe("Using common setup", () => {
+  let bookingForBeforeAll: number;
+  let bookingForBeforeEach: number;
+  const seedNumber = Math.floor(Date.now() / 1000).toString();
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  test.beforeAll(async ({ request }) => {
+    let response = await request.post(
+      "https://restful-booker.herokuapp.com/booking",
+      {
+        data: {
+          firstname: "Mary Jane",
+          lastname: `Watson${seedNumber}`,
+          totalprice: 120,
+          depositpaid: true,
+          bookingdates: {
+            checkin: "2018-01-01",
+            checkout: "2019-01-01",
+          },
+          additionalneeds: "Breakfast",
+        },
+        headers: {
+          Accept: "application/json",
+        },
+      }
+    );
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+    expect(response.status(), "testing whether status is 200").toBe(200);
+    const BodyPost = await response.json();
+    bookingForBeforeAll = BodyPost.bookingid;
+    console.log(`Booking for beforeAll: ${bookingForBeforeAll}`);
+  });
 
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/);
+  test.beforeEach(async ({ request }) => {
+    let response = await request.post(
+      "https://restful-booker.herokuapp.com/booking",
+      {
+        data: {
+          firstname: "Peter",
+          lastname: `Parker${seedNumber}`,
+          totalprice: 130,
+          depositpaid: true,
+          bookingdates: {
+            checkin: "2018-01-01",
+            checkout: "2019-01-01",
+          },
+          additionalneeds: "Flies",
+        },
+        headers: {
+          Accept: "application/json",
+        },
+      }
+    );
+
+    expect(response.status(), "testing whether status is 200").toBe(200);
+    const BodyPost = await response.json();
+    bookingForBeforeEach = BodyPost.bookingid;
+    console.log(`Booking for beforeEach: ${bookingForBeforeEach}`);
+  });
+
+  test.afterEach(async ({ request }) => {
+    const responseDel = await request.delete(
+      `https://restful-booker.herokuapp.com/booking/${bookingForBeforeEach}`,
+      {
+        headers: {
+          // 'Cookie': `token=${Token}`
+          Authorization: "Basic YWRtaW46cGFzc3dvcmQxMjM=",
+        },
+      }
+    );
+
+    expect(
+      responseDel.status(),
+      "testing whether status is 201 (means it was successfully deleted)."
+    ).toBe(201);
+  });
+
+  test.afterAll(async ({ request }) => {
+    const responseDel = await request.delete(
+      `https://restful-booker.herokuapp.com/booking/${bookingForBeforeAll}`,
+      {
+        headers: {
+          // 'Cookie': `token=${Token}`
+          Authorization: "Basic YWRtaW46cGFzc3dvcmQxMjM=",
+        },
+      }
+    );
+
+    expect(
+      responseDel.status(),
+      "testing whether status is 201 (means it was successfully deleted)."
+    ).toBe(201);
+  });
+
+  test("Search for a booking and if it is found, delete another one", async ({
+    request,
+  }) => {
+    const responseFirstBooking = await request.get(
+      `https://restful-booker.herokuapp.com/booking`,
+      {
+        params: {
+          firstname: "Mary Jane",
+          lastname: `Watson${seedNumber}`,
+        },
+      }
+    );
+
+    expect(responseFirstBooking.status(), "testing whether status is 200").toBe(
+      200
+    );
+    const BodyFirstPost: any[] = await responseFirstBooking.json();
+    const BookingIdFirst = BodyFirstPost[0].bookingid;
+    console.log(`First booking: ${BookingIdFirst}`);
+
+    const responseSecondBooking = await request.get(
+      `https://restful-booker.herokuapp.com/booking`,
+      {
+        params: {
+          firstname: "Peter",
+          lastname: `Parker${seedNumber}`,
+        },
+      }
+    );
+
+    expect(
+      responseSecondBooking.status(),
+      "testing whether status is 200"
+    ).toBe(200);
+    const BodySecondPost: any[] = await responseSecondBooking.json();
+    const BookingIdSecond = BodySecondPost[0].bookingid;
+    console.log(`First booking: ${BookingIdSecond}`);
+  });
 });
