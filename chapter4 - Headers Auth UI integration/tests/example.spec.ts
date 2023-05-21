26b4f986ef311da6644ac2d06bd0bae9751da4c8
André Cordazzo Brunelli <andre.cordazzo-brunelli@sogeti.no>
2023-05-21 19:58:36 +0200

    chapter4

---
+++ chapter4 - Headers Auth UI integration/tests/example.spec.ts
@@ -0,0 +1,128 @@
+import { test, expect } from "@playwright/test";
+import Ajv from "ajv";
+import { iPet, schemaPet, schemaUser } from "../resources/interfaces";
+const ajv = new Ajv();
+
+const username = "andbru";
+const password = "Password123";
+
+test.beforeAll(async ({ request }) => {
+  let response = await request.post("user", {
+    data: {
+      username: username,
+      firstName: "andre",
+      lastName: "brunelli",
+      email: "andre@fakeemail.com",
+      password: password,
+      phone: "987654321",
+    },
+    headers: {
+      accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  });
+  expect(response.status(), "Is user registration successfull").toBe(200);
+
+  response = await request.get(`user/${username}`, {
+    headers: {
+      accept: "application/json",
+    },
+  });
+  const user = await response.json();
+  const valid = ajv.validate(schemaUser, user);
+  expect(valid, "Validating login schema").toBeTruthy();
+});
+
+test.beforeEach(async ({ request }) => {
+  let response = await request.get("user/login", {
+    params: {
+      username: username,
+      password: password,
+    },
+    headers: {
+      Accept: "application/json",
+    },
+  });
+
+  //saving storage state
+  test.fail();
+
+  expect(response.status(), "Login").toBe(200);
+});
+
+test.afterEach(async ({ request }) => {
+  let response = await request.get("user/logout", {});
+  expect(response.status(), "Logout").toBe(200);
+});
+
+test.afterAll(async ({ request }) => {
+  let response = await request.delete(`user/${username}`, {});
+  expect(response.status(), "User deletion").toBe(200);
+});
+
+test("Add pet with API, delete with UI and verify on API", async ({
+  request,
+  browser,
+}) => {
+  let petId: number;
+  await test.step("POST pet", async () => {
+    let response = await request.post(`pet`, {
+      headers: {
+        accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      data: {
+        id: 12345678,
+        category: {
+          id: 0,
+          name: "string",
+        },
+        name: "Baguette",
+        photoUrls: ["a", "b"],
+        tags: [
+          {
+            id: 0,
+            name: "French Bulldog",
+          },
+          {
+            id: 1,
+            name: "lazy",
+          },
+        ],
+        status: "available",
+      },
+    });
+
+    // validate response
+    const pet: iPet = await response.json();
+    petId = pet.id;
+    console.log(`pet id: ${petId}`);
+    const validate = ajv.compile<iPet>(schemaPet);
+    const valid = validate(pet);
+    if (valid) {
+      // data is Foo here
+      console.log("valid");
+    } else {
+      console.log(validate.errors);
+    }
+    expect(valid, "Validating schema for new pet").toBeTruthy();
+
+    // verifying that pet is added
+    response = await request.get(`pet/${petId}`,{
+      headers: {
+        accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    });
+    expect(response.ok, `Pet id ${petId} is added`).toBeTruthy();
+  });
+
+  await test.step("DELETE pet", async () => {
+    test.fail();
+  });
+
+  await test.step("GET pet returns 404", async () => {
+
+    test.fail();
+  });
+});
